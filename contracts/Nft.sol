615c09d83b1e36eda58572bfd3b4c903036d51f6
Michael Wilkowski <wilks7@icloud.com>
2022-11-04 14:25:48 -0400

    Yarn

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -1,6 +1,6 @@
-// // SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: UNLICENSED
 
-// pragma solidity ^0.8.0;
+pragma solidity ^0.8.0;
 
 // import "@openzeppelin/contracts/utils/Strings.sol";
 // import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
